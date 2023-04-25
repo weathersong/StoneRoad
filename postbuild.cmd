88ab3748323d59c51a600f1e601505f0220d6f35
dfrauzel <dfrauzel@pm.me>
2023-04-25 17:48:30 -0600

    When using robocopy in a postbuild.cmd, add "exit 0" at the bottom of the cmd because robocopy has a success code of 1.

--- postbuild.cmd
+++ postbuild.cmd
@@ -3,3 +3,6 @@ copy ".\bin\Debug\netstandard2.0\StoneRoad.pdb" ".\"
 del ".\bin\Debug\netstandard2.0\*.dll"
 
 robocopy /MIR "D:\Games\Vintage Story\_ Builds\StoneRoad" "D:\Games\Vintage Story\_ BuildsOut\StoneRoad" /XD .git .vs bin obj StoneRoad /XF .git* postbuild.cmd StoneRoad.csproj StoneRoad.sln
+
+:: robocopy normally (successfully) exits with code 1, so...
+exit 0
