ab2aa5aec6b8a39922431ccb9fb020f4a2afeb37
dfrauzel <dfrauzel@pm.me>
2023-04-25 17:45:23 -0600

    Local cleanup.

--- postbuild.cmd
+++ postbuild.cmd
@@ -1,3 +1,5 @@
 copy ".\bin\Debug\netstandard2.0\StoneRoad.dll" ".\"
 copy ".\bin\Debug\netstandard2.0\StoneRoad.pdb" ".\"
 del ".\bin\Debug\netstandard2.0\*.dll"
+
+robocopy /MIR "D:\Games\Vintage Story\_ Builds\StoneRoad" "D:\Games\Vintage Story\_ BuildsOut\StoneRoad" /XD .git .vs bin obj StoneRoad /XF .git* postbuild.cmd StoneRoad.csproj StoneRoad.sln
